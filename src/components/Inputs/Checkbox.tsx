2c92ed7fb59cf1469287be6227487b4c909672b3
sarinaem <sarinaemadi137513@yahoo.com>
2025-08-05 11:58:49 +0330

    Save name of project

--- src/components/Inputs/Checkbox.tsx
+++ src/components/Inputs/Checkbox.tsx
@@ -1,23 +1,24 @@
 import Checkbox from '@mui/material/Checkbox';
 import CheckIcon from '@mui/icons-material/Check';
-import type { CheckboxProps } from '../../types/input'; // اگر این تایپ در جای درستی تعریف شده باشد
 
-const CheckBox = (props: CheckboxProps) => {
-  return (
-    <div className="flex items-center gap-3">
-      <Checkbox
-        {...props} 
-        icon={
-          <span className="block w-5 h-5 rounded border border-gray-300" />
-        }
-        checkedIcon={
-          <span className="flex items-center justify-center w-5 h-5 rounded bg-primary-blueCheck">
-            <CheckIcon sx={{ fontSize: 14, color: 'white' }} />
-          </span>
-        }
-      />
-    </div>
-  );
+const CheckBox = ({ label }: { label: string }) => {
+    return (
+        <div className="flex items-center gap-3">
+            <Checkbox
+                icon={<span className="block w-5 h-5 rounded border border-gray-300" />}
+                checkedIcon={
+                    <span className="flex items-center justify-center w-5 h-5 rounded bg-primary-blueCheck">
+                        <CheckIcon sx={{ fontSize: 14, color: 'white' }} />
+                    </span>
+                }
+                sx={{ padding: 0 }}
+                defaultChecked
+            />
+            <label className="font-peyda font-medium text-xl-custom text-white">
+                {label}
+            </label>
+        </div>
+    );
 };
 
 export default CheckBox;
