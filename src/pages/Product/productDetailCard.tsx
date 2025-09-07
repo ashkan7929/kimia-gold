9cd410f5fae41a1a32a8147e34757a7eb1278d88
sarinaem <sarinaemadi137513@yahoo.com>
2025-09-07 13:59:25 +0330

    [edit] productDetail

--- src/pages/Product/productDetailCard.tsx
+++ src/pages/Product/productDetailCard.tsx
@@ -10,102 +10,207 @@ import {
 } from '../../Icons';
 import CheckBox from '../../components/Inputs/Checkbox';
 import Button from '../../components/Button/Button';
+import SegmentedButtons from '../../components/Button/ButtonGroups';
+import { Controller, useForm } from 'react-hook-form';
+import TextField from '../../components/Inputs/TextField';
+import DateField from '../../components/Inputs/Datepiker';
+import Tabel from '../../components/Inputs/Tabel';
+import { Link } from 'react-router-dom';
 
 const ProductDetailCard = () => {
-  return (
-    <div className="bg-primary-darker dark:bg-primary-darker/90">
-      <div className="mx-3 mt-2 flex items-center justify-between">
-        <Typography className="!font-peyda text-text-color dark:text-white" fontSize={12}>
-          آیا بیمه‌شده با بیمه‌گذار یکی است؟
-        </Typography>
-        <ToggleButton />
-      </div>
+    const { control } = useForm();
 
-      <Disclosure as="div" className="p-1">
-        {({ open }) => (
-          <div className="w-full rounded-lg bg-custom-bg-card dark:bg-white/5 transition-all duration-700">
-            <Disclosure.Button className="flex w-full cursor-pointer items-center justify-between p-4">
-              <span className="font-peyda text-xs text-text-color dark:text-white">
-                تکمیل اطلاعات هویتی
-              </span>
-              {open ? <FaChevronUp fontSize={12} /> : <FaChevronDown fontSize={12} />}
-            </Disclosure.Button>
-
-            <Transition
-              enter="transition transition-[max-height] duration-500 ease-in"
-              enterFrom="transform max-h-0"
-              enterTo="transform max-h-[200vh]"
-              leave="transition transition-[max-height] duration-500 ease-out"
-              leaveFrom="transform max-h-screen"
-              leaveTo="transform max-h-0"
-            >
-              <Disclosure.Panel as="div" className="overflow-hidden">
-                <div className="flex flex-col gap-3 p-4">
-                  <div>
-                    <label className="mb-2 block font-peyda text-micro font-semibold leading-normal text-text-color dark:text-white">
-                      قد (سانتی‌متر)
-                    </label>
-                    <TextField mobileIcon={<CiMobile3 />} placeholder="لطفاً قد خود را وارد کنید" />
-                  </div>
-
-                  <div>
-                    <label className="mb-2 block font-peyda text-micro font-semibold leading-normal text-text-color dark:text-white">
-                      وزن
-                    </label>
-                    <TextField mobileIcon={<CiMobile3 />} placeholder="لطفاً وزن خود را وارد کنید" />
-                  </div>
-
-                  <div className="flex items-center text-text-color dark:text-white">
-                    <CheckBox label="آیا سیگار می‌کشید؟" defaultChecked />
-                  </div>
+    return (
+        <div className="bg-primary-darker light:bg-white">
+            <div className="mx-3 flex items-center justify-between mt-4">
+                <Typography
+                    className="!font-peyda text-text-color light:text-light-text-color"
+                    fontSize={12}
+                >
+                    آیا بیمه‌شده با بیمه‌گذار یکی است؟
+                </Typography>
+                <div className="mt-2">
+                    <ToggleButton />
                 </div>
-              </Disclosure.Panel>
-            </Transition>
-          </div>
-        )}
-      </Disclosure>
+            </div>
 
-      <Disclosure as="div" className="p-1">
-        {({ open }) => (
-          <div className="w-full rounded-lg bg-custom-bg-card dark:bg-white/5 transition-all duration-700">
-            <Disclosure.Button className="flex w-full cursor-pointer items-center justify-between p-4">
-              <span className="font-peyda text-xs text-text-color dark:text-white">
-                تکمیل اطلاعات بیمه‌ای
-              </span>
-              {open ? <FaChevronUp fontSize={12} /> : <FaChevronDown fontSize={12} />}
-            </Disclosure.Button>
+            {/* سکشن 1: اطلاعات هویتی */}
+            <Disclosure as="div" className="p-1">
+                {({ open }) => (
+                    <div className="w-full rounded-lg bg-custom-bg-card light:bg-white transition-all duration-700">
+                        <Disclosure.Button className="flex w-full cursor-pointer items-center justify-between p-4">
+                            <span className="font-peyda text-xs text-text-color light:text-light-text-color">
+                                تکمیل اطلاعات هویتی
+                            </span>
+                            {open ? (
+                                <FaChevronUp
+                                    className="text-text-color light:text-light-text-color"
+                                    fontSize={12}
+                                />
+                            ) : (
+                                <FaChevronDown
+                                    className="text-text-color light:text-light-text-color"
+                                    fontSize={12}
+                                />
+                            )}
+                        </Disclosure.Button>
 
-            <Transition
-              enter="transition transition-[max-height] duration-500 ease-in"
-              enterFrom="transform max-h-0"
-              enterTo="transform max-h-[200vh]"
-              leave="transition transition-[max-height] duration-500 ease-out"
-              leaveFrom="transform max-h-screen"
-              leaveTo="transform max-h-0"
-            >
-              <Disclosure.Panel as="div" className="overflow-hidden">
-                <div className="flex flex-col gap-3 p-4">
-                  <div>
-                    <label className="mb-2 block font-peyda text-micro font-semibold leading-normal text-text-color dark:text-white">
-                      شماره بیمه‌نامه قبلی (اختیاری)
-                    </label>
-                    <TextField placeholder="" />
-                  </div>
-                  <div className="flex items-center text-text-color dark:text-white">
-                    <CheckBox label="آیا سابقه بیمه عمر دارید؟" />
-                  </div>
-                </div>
-              </Disclosure.Panel>
-            </Transition>
-          </div>
-        )}
-      </Disclosure>
+                        <Transition
+                            enter="transition transition-[max-height] duration-500 ease-in"
+                            enterFrom="transform max-h-0"
+                            enterTo="transform max-h-[200vh]"
+                            leave="transition transition-[max-height] duration-500 ease-out"
+                            leaveFrom="transform max-h-screen"
+                            leaveTo="transform max-h-0"
+                        >
+                            <Disclosure.Panel as="div" className="overflow-hidden">
+                                <div className="flex flex-col gap-3 p-4">
+                                    <div>
+                                        <label className="mb-2 block font-peyda text-xs font-semibold leading-normal text-text-color dark:text-white">
+                                            قد (سانتی‌متر)
+                                        </label>
+                                        <TextField
+                                            mobileIcon={<FaWeightScale />}
+                                            placeholder="لطفاً قد خود را وارد کنید"
+                                            className="placeholder:text-micro"
+                                        />
+                                    </div>
 
-      <Button className="font-peyda mt-4 w-full rounded-md bg-primary-blue text-sm text-white">
-        ثبت اطلاعات
-      </Button>
-    </div>
-  );
+                                    <div>
+                                        <label className="mb-2 block font-peyda text-xs font-semibold leading-normal text-text-color dark:text-white">
+                                            وزن
+                                        </label>
+                                        <TextField
+                                            mobileIcon={<GiBodyHeight />}
+                                            placeholder="لطفاً وزن خود را وارد کنید"
+                                        />
+                                    </div>
+
+                                    <div className="flex items-center text-text-color dark:text-white">
+                                        <CheckBox label="آیا سیگار می‌کشید؟" defaultChecked />
+                                    </div>
+                                </div>
+                            </Disclosure.Panel>
+                        </Transition>
+                    </div>
+                )}
+            </Disclosure>
+
+            <Disclosure as="div" className="p-1">
+                {({ open }) => (
+                    <div className="w-full rounded-lg bg-custom-bg-card light:bg-white transition-all duration-700">
+                        <Disclosure.Button className="flex w-full cursor-pointer items-center justify-between p-4">
+                            <span className="font-peyda text-xs text-text-color light:text-light-text-color">
+                                تکمیل اطلاعات بیمه‌ای
+                            </span>
+                            {open ? (
+                                <FaChevronUp
+                                    className="text-text-color light:text-light-text-color"
+                                    fontSize={12}
+                                />
+                            ) : (
+                                <FaChevronDown
+                                    className="text-text-color light:text-light-text-color"
+                                    fontSize={12}
+                                />
+                            )}
+                        </Disclosure.Button>
+
+                        <Transition
+                            enter="transition transition-[max-height] duration-500 ease-in"
+                            enterFrom="transform max-h-0"
+                            enterTo="transform max-h-[200vh]"
+                            leave="transition transition-[max-height] duration-500 ease-out"
+                            leaveFrom="transform max-h-screen"
+                            leaveTo="transform max-h-0"
+                        >
+                            <Disclosure.Panel as="div" className="">
+                                <div className="flex flex-col gap-5 p-4">
+                                    <SegmentedButtons
+                                        className="w-full"
+                                        buttons={[
+                                            { label: 'برای خودم' },
+                                            { label: 'کاربر جدید' },
+                                            { label: 'مشتریان قبلی' },
+                                        ]}
+                                        defaultIndex={0}
+                                    />
+                                    <div className="">
+                                        <Typography
+                                            className="text-white light:text-light-text-color !font-peyda mb-2"
+                                            fontSize={12}
+                                        >
+                                            تاریخ تولد *
+                                        </Typography>
+                                        <Controller
+                                            name="birthDate"
+                                            control={control}
+                                            render={({ field }) => (
+                                                <DateField
+                                                    mobileIcon={<CiCalendarDate />}
+                                                    placeholder="تاریخ تولد"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                    onBlur={field.onBlur}
+                                                    name={field.name}
+                                                    className="w-full"
+                                                />
+                                            )}
+                                        />
+                                    </div>
+
+                                    <div className="flex items-center gap-0 text-sm text-text-color light:text-light-bg-input">
+                                        <CheckBox defaultChecked label="آیا بیماری خاصی دارید؟" />
+                                    </div>
+                                    <Tabel className="w-full border-collapse text-right font-peyda">
+                                        <thead>
+                                            <tr className="bg-light-primary-darker light:bg-primary-darker">
+                                                <th
+                                                    scope="col"
+                                                    className="border border-gray-300 text-sm text-center light:border-gray-700 light:text-white px-2 py-1"
+                                                >
+                                                    لیست بیماری‌ها
+                                                </th>
+                                            </tr>
+                                        </thead>
+
+                                        <tbody className="">
+                                            <tr className="light:bg-light-primary-darker bg-primary-darker">
+                                                <td className="border border-gray-300 text-text-color light:text-light-text-color text-xs text-center  light:border-gray-700 px-4 py-2">
+                                                    انواع سرطان‌ها مثل سرطان معده
+                                                </td>
+                                            </tr>
+                                            <tr className="light:bg-light-primary-darker bg-primary-darker">
+                                                <td className="border border-gray-300 text-text-color light:text-light-text-color text-xs text-center light:border-gray-700 px-4 py-2">
+                                                    صرع
+                                                </td>
+                                            </tr>
+                                            <tr className="light:bg-light-primary-darker bg-primary-darker">
+                                                <td className="border border-gray-300 text-text-color light:text-light-text-color text-xs text-center light:border-gray-700 px-4 py-2">
+                                                    آسم
+                                                </td>
+                                            </tr>
+                                            <tr className="light:bg-light-primary-darker bg-primary-darker">
+                                                <td className="border border-gray-300 text-text-color light:text-light-text-color text-xs text-center light:border-gray-700 px-4 py-2">
+                                                    زخم معده
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </Tabel>
+                                </div>
+                            </Disclosure.Panel>
+                        </Transition>
+                    </div>
+                )}
+            </Disclosure>
+
+            <Link to="/paymentInsurance">
+                <Button className="font-peyda mt-4 w-full rounded-md bg-primary-blue text-sm text-white">
+                    ثبت اطلاعات
+                </Button>
+            </Link>
+        </div>
+    );
 };
 
 export default ProductDetailCard;
