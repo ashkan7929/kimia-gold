905efd4d9ed9739e3b9a6a5a47c52832bace7631
sarinaem <sarinaemadi137513@yahoo.com>
2025-10-04 18:05:28 +0330

    write test for checkbox and give success result

---
+++ src/components/Inputs/Checkbox.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import Checkbox from './Checkbox';
+import '@testing-library/jest-dom';
+
+describe('Checkbox', () => {
+    test('test is success when active', () => {
+        render(<Checkbox label="قوانین را می‌پذیرم" />);
+
+        // show label
+        expect(screen.getByText('قوانین را می‌پذیرم')).toBeInTheDocument();
+
+        //has checkbox
+        const checkbox = screen.getByRole('checkbox');
+        expect(checkbox).toBeInTheDocument();
+
+        // not select
+        expect(checkbox).not.toBeChecked();
+
+        // test('با defaultChecked تیک خورده است', () => {
+        //     render(<Checkbox label="قوانین را می‌پذیرم" defaultChecked />);
+        //     expect(screen.getByRole('checkbox')).toBeChecked();
+        // });
+    });
+});
